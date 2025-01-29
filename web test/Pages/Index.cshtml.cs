bf2a8f32d146d8df668a5e065a5416206467fcc5
Nickolay <druumm36@gmail.com>
2025-01-29 11:09:55 +0300

    test_1

--- web test/Pages/Index.cshtml.cs
+++ web test/Pages/Index.cshtml.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Numerics;
 
 namespace web_test.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public string Message { get; private set; } = "";
+        public void OnGet(string name = "Vasia", int age = 123)
         {
+            Message = $"Name: {name} и ему {age} лет";
         }
     }
 }
