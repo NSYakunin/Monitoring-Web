e52197385fdcea9a588db40c3127e96d322b6921
Nickolay <druumm36@gmail.com>
2025-02-06 13:43:20 +0300

    Создание отчета в PDF 2

--- web test/ReportGenerator.cs
+++ web test/ReportGenerator.cs
@@ -19,7 +19,7 @@ namespace web_test
                     page.Size(PageSizes.A4.Landscape()); // Горизонтальная ориентация
                     page.Margin(1, Unit.Centimetre);
                     page.PageColor(Colors.White);
-                    page.DefaultTextStyle(x => x.FontSize(12));
+                    page.DefaultTextStyle(x => x.FontSize(8));
 
                     page.Header()
                         .Text(title)
@@ -35,48 +35,64 @@ namespace web_test
                             table.ColumnsDefinition(columns =>
                             {
                                 columns.RelativeColumn(2); // Номер работы
-                                columns.RelativeColumn(8); // Название документа (шире)
-                                columns.RelativeColumn(7); // Название работы (шире)
+                                columns.RelativeColumn(6); // Название документа (шире)
+                                columns.RelativeColumn(5.5f); // Название работы (шире)
                                 columns.RelativeColumn(2); // Исполнитель
                                 columns.RelativeColumn(2); // Контролирующий
-                                columns.RelativeColumn(2); // Принимающий
-                                columns.RelativeColumn(); // Планонвая дата
-                                columns.RelativeColumn(); // Корр1
-                                columns.RelativeColumn(); // Корр2
-                                columns.RelativeColumn(); // Корр3
-                                columns.RelativeColumn(); // Факт
+                                columns.RelativeColumn(2.2f); // Принимающий
+                                columns.RelativeColumn(1.5f); // Планонвая дата
+                                columns.RelativeColumn(1.5f); // Корр1
+                                columns.RelativeColumn(1.5f); // Корр2
+                                columns.RelativeColumn(1.5f); // Корр3
+                                columns.RelativeColumn(1.5f); // Факт
+                                columns.RelativeColumn(1.5f); // Подпись
                             });
 
                             table.Header(header =>
                             {
-                                header.Cell().Background(Colors.Grey.Lighten2).Text("Номер");
-                                header.Cell().Background(Colors.Grey.Lighten2).Text("Название документа");
-                                header.Cell().Background(Colors.Grey.Lighten2).Text("Название работы");
-                                header.Cell().Background(Colors.Grey.Lighten2).Text("Исполнитель");
-                                header.Cell().Background(Colors.Grey.Lighten2).Text("Контроль");
-                                header.Cell().Background(Colors.Grey.Lighten2).Text("Принимающий");
-                                header.Cell().Background(Colors.Grey.Lighten2).Text("План");
-                                header.Cell().Background(Colors.Grey.Lighten2).Text("Корр1");
-                                header.Cell().Background(Colors.Grey.Lighten2).Text("Корр2");
-                                header.Cell().Background(Colors.Grey.Lighten2).Text("Корр3");
-                                header.Cell().Background(Colors.Grey.Lighten2).Text("Факт");
+                                header.Cell().Background(Colors.Grey.Lighten2).Element(Block).Text("Номер");
+                                header.Cell().Background(Colors.Grey.Lighten2).Element(Block).Text("Название документа");
+                                header.Cell().Background(Colors.Grey.Lighten2).Element(Block).Text("Название работы");
+                                header.Cell().Background(Colors.Grey.Lighten2).Element(Block).Text("Исполнитель");
+                                header.Cell().Background(Colors.Grey.Lighten2).Element(Block).Text("Контроль");
+                                header.Cell().Background(Colors.Grey.Lighten2).Element(Block).Text("Принимающий");
+                                header.Cell().Background(Colors.Grey.Lighten2).Element(Block).Text("План");
+                                header.Cell().Background(Colors.Grey.Lighten2).Element(Block).Text("Корр1");
+                                header.Cell().Background(Colors.Grey.Lighten2).Element(Block).Text("Корр2");
+                                header.Cell().Background(Colors.Grey.Lighten2).Element(Block).Text("Корр3");
+                                header.Cell().Background(Colors.Grey.Lighten2).Element(Block).Text("Факт");
+                                header.Cell().Background(Colors.Grey.Lighten2).Element(Block).Text("Подпись");
                             });
-
                             foreach (var item in data)
                             {
-                                table.Cell().Text(item.DocumentNumber);
-                                table.Cell().Text(item.DocumentName);
-                                table.Cell().Text(item.WorkName);
-                                table.Cell().Text(item.Executor);
-                                table.Cell().Text(item.Controller);
-                                table.Cell().Text(item.Approver);
-                                table.Cell().Text(item.PlanDate.Value);
-                                table.Cell().Text("        ");
-                                table.Cell().Text("        ");
-                                table.Cell().Text("        ");
-                                table.Cell().Text("        ");
+                                table.Cell().Element(Block).Text(item.DocumentNumber);
+                                table.Cell().Element(Block).Text(item.DocumentName);
+                                table.Cell().Element(Block).Text(item.WorkName);
+                                table.Cell().Element(Block).Text(item.Executor);
+                                table.Cell().Element(Block).Text(item.Controller);
+                                table.Cell().Element(Block).Text(item.Approver);
+                                table.Cell().Element(Block).Text(item.PlanDate?.ToString("dd.MM.yyyy") ?? "");
+                                table.Cell().Element(Block).Text(item.Korrect1?.ToString("dd.MM.yyyy") ?? "");
+                                table.Cell().Element(Block).Text(item.Korrect2?.ToString("dd.MM.yyyy") ?? "");
+                                table.Cell().Element(Block).Text(item.Korrect3?.ToString("dd.MM.yyyy") ?? "");
+                                table.Cell().Element(Block).Text(item.FactDate?.ToString("dd.MM.yyyy") ?? "");
+                                table.Cell().Element(Block).Text("    ");
                             }
                         });
+
+                    static IContainer Block(IContainer container)
+                    {
+                        return container
+                            .Border(0.5f)
+                            .ShowOnce()
+                            .MinWidth(20)
+                            .MinHeight(20)
+                            .AlignCenter()
+                            .AlignMiddle()
+                            .ScaleToFit()
+                            .ScaleHorizontal(1)
+                            ;
+                    }
                 });
             })
             .GeneratePdf(filePath); // Сохраняем файл в wwwroot/report.pdf
