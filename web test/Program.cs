47ff2c9d135dacfa04f709d4303f2c696b3ea859
Nickolay <druumm36@gmail.com>
2025-02-13 08:54:15 +0300

    Правки с рабочим сервером

--- web test/Program.cs
+++ web test/Program.cs
@@ -2,17 +2,21 @@ using Monitoring.Application.Interfaces;
 using Monitoring.Infrastructure.Services;
 using Microsoft.Extensions.Configuration;
 using Monitoring.Infrastructure.Services.Fake;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.ConfigureFilter(new IgnoreAntiforgeryTokenAttribute());
+});
 
 builder.Services.AddMemoryCache();
 
-// builder.Services.AddScoped<IWorkItemService, WorkItemService>();
-builder.Services.AddScoped<IWorkItemService, FakeWorkItemService>();
-// builder.Services.AddScoped<ILoginService, LoginService>();
-builder.Services.AddScoped<ILoginService, FakeLoginService>();
+ builder.Services.AddScoped<IWorkItemService, WorkItemService>();
+//builder.Services.AddScoped<IWorkItemService, FakeWorkItemService>();
+ builder.Services.AddScoped<ILoginService, LoginService>();
+//builder.Services.AddScoped<ILoginService, FakeLoginService>();
 
 builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
